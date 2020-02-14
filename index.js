c7e255da739ec9cbd29bddf5a1a0c779355bc24a
Jan Häusler <jan.haeusler@rwth-aachen.de>
2020-02-14 23:44:08 +0100

    Convert to new api (?).

--- index.js
+++ index.js
@@ -5,28 +5,15 @@ const to = require("await-to-js").default;
 const moment = require("moment");
 
 // NOTE: only for anything except China and USA
-const ROW_SERVER = "b2vapi.bmwgroup.com";
-const AUTH_URL = `https://${ROW_SERVER}/gcdm/oauth/token`;
+const AUTH_SERVER = "customer.bmwgroup.com";
+const AUTH_URL = `https://${AUTH_SERVER}/gcdm/oauth/authenticate`;
 
-const auth_headers = {
-  "Content-Type": "application/x-www-form-urlencoded",
-  "Content-Length": "124",
-  Connection: "Keep-Alive",
-  Host: "b2vapi.bmwgroup.com",
-  "Accept-Encoding": "gzip",
-  Authorization:
-    "Basic blF2NkNxdHhKdVhXUDc0eGYzQ0p3VUVQOjF6REh4NnVuNGNEanli" +
-    "TEVOTjNreWZ1bVgya0VZaWdXUGNRcGR2RFJwSUJrN3JPSg==",
-  Credentials:
-    "nQv6CqtxJuXWP74xf3CJwUEP:1zDHx6un4cDjybLENN3kyfumX2kEYigWPcQpdvDRpIBk7rOJ",
-  "User-Agent": "okhttp/2.60"
-};
-
-const BASE_URL = `https://${ROW_SERVER}/webapi/v1`;
-const VEHICLES_URL = `${BASE_URL}/user/vehicles`;
+const API_SERVER = "www.bmw-connecteddrive.com";
+const API_BASE_URL = `https://${API_SERVER}/api`;
+const VEHICLES_URL = `${API_BASE_URL}/me/vehicles/v2?all=true&brand=BM`;
 const SINGLE_VEHICLE_URL = `${VEHICLES_URL}/%s`;
 const REMOTE_SERVICE_URL = `${SINGLE_VEHICLE_URL}/executeService`;
-const VEHICLE_STATUS_URL = `${SINGLE_VEHICLE_URL}/status`;
+const VEHICLE_STATUS_URL = `${API_BASE_URL}/vehicle/dynamic/v1/%s?offset=-60`;
 
 module.exports = {
   async auth(username, password) {
@@ -35,38 +22,46 @@ module.exports = {
     let vehicles = [];
 
     const values = {
-      grant_type: "password",
-      scope: "authenticate_user vehicle_data remote_services",
-      username: username,
-      password: password
+      client_id: "dbf0a542-ebd1-4ff0-a9a7-55172fbfce35",
+      redirect_uri:
+        "https://www.bmw-connecteddrive.com/app/static/external-dispatch.html",
+      response_type: "token",
+      scope: "authenticate_user fupo",
+      username,
+      password
     };
 
     console.log("Authenticating...");
     let err;
     let result = await axios.post(AUTH_URL, qs.stringify(values), {
-      headers: auth_headers
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
     });
 
-    access_token = result.data.access_token;
+    const token_data = qs.parse(
+      new URL(result.request.res.responseUrl).hash.slice(1)
+    );
+
+    access_token = token_data.access_token;
     token_expires_in = new Date(
-      new Date().getTime() + 1000 * result.data.expires_in
+      new Date().getTime() + 1000 * token_data.expires_in
     );
 
     const headers = {
-      Authorization: `Bearer ${access_token}`,
-      referer: "https://www.bmw-connecteddrive.de/app/index.html"
+      authorization: `Bearer ${access_token}`
     };
 
     return {
       async vehicles() {
         console.log("Getting vehicles...");
         result = await axios.get(VEHICLES_URL, { headers: headers });
-        return result.data.vehicles;
+        return result.data;
       },
       async findVehicle(vin) {
         console.log("Getting vehicles...");
         result = await axios.get(VEHICLES_URL, { headers: headers });
-        vehicles = result.data.vehicles; //.map(vehicle => ({ vin: vehicle.vin })));
+        vehicles = result.data; //.map(vehicle => ({ vin: vehicle.vin })));
 
         let vehicle = vehicles.find(vehicle => vehicle.vin == vin);
 
@@ -96,17 +91,14 @@ module.exports = {
             console.log(`Requesting status for ${vehicle.vin}...`);
             [err, result] = await to(
               axios.get(util.format(VEHICLE_STATUS_URL, vehicle.vin), {
-                headers: headers,
-                params: {
-                  deviceTime: moment().format("YYYY-MM-DDTHH:mm:ss")
-                }
+                headers: headers
               })
             );
             if (err) {
               throw Error(err);
             }
 
-            return result.data.vehicleStatus;
+            return result.data.attributesMap;
           }
         };
       }
